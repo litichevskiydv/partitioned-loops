98758fa0488de35bc420bac294e5f0cc9e44003e
mapogolions <ivanovpavelalex45@gmail.com>
2020-01-05 20:40:22 +0500

    Some more duplicate code

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ function* range(fromInclusive, toExclusive) {
   while (fromInclusive < toExclusive) yield fromInclusive++;
 }
 
-module.exports.forEachAsync = async function(source, body, loopState) {
+async function forEachAsync(source, body, loopState) {
   return await new Promise(resolve => {
     forEachBody(source[Symbol.iterator](), body, loopState || {}, resolve);
   });
 };
 
-module.exports.forAsync = async function(fromInclusive, toExclusive, body, loopState) {
-  return await new Promise(resolve => {
-    forEachBody(range(fromInclusive, toExclusive), body, loopState || {}, resolve);
-  });
+async function forAsync(fromInclusive, toExclusive, body, loopState) {
+  return await forEachAsync(range(fromInclusive, toExclusive), body, loopState);
 };
+
+module.exports = { forAsync, forEachAsync};
